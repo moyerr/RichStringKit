d2a77384963a390bf3046bf35cb6a792be31b50d
Robbie Moyer <rmoyer08@gmail.com>
2022-09-14 11:50:08 -0400

    Fix watchOS version in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RichStringKit",
-    platforms: [.macOS(.v12), .macCatalyst(.v15), .iOS(.v15), .watchOS(.v7), .tvOS(.v15)],
+    platforms: [.macOS(.v12), .macCatalyst(.v15), .iOS(.v15), .watchOS(.v8), .tvOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
